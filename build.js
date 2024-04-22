3954c0fa1109674973dfa9e559be7e5a6bf77e76
zce <w@zce.me>
2024-04-22 18:07:25 +0800

    feat: initial commit

---
+++ build.js
@@ -0,0 +1,71 @@
+// @ts-check
+
+/**
+ * @typedef {{ name: string, svg: string, base64: string }} Icon
+ */
+
+import { mkdir, readFile, readdir, writeFile } from 'node:fs/promises'
+import { basename } from 'node:path'
+
+/**
+ * search svg files in a directory
+ * @param {string} dir svgs directory
+ * @returns {Promise<string[]>} a list of svg filenames
+ */
+const searchSvgs = async (dir) => {
+  const dirents = await readdir(dir, { withFileTypes: true })
+  return dirents.filter(d => d.isFile() && d.name.endsWith('.svg')).map(d => `${dir}/${d.name}`)
+}
+
+/**
+ * load an svg file
+ * @param {string} path svg filename
+ * @returns {Promise<Icon>} An icon object
+ */
+const loadSvg = async (path) => {
+  const camel = basename(path, '.svg').replace(/-([a-z0-9])/g, (_, letter) => letter.toUpperCase())
+  const name = camel[0].toUpperCase() + camel.slice(1)
+  const svg = await readFile(path,'utf8')
+  const base64 = Buffer.from(svg).toString('base64')
+  return { name, svg, base64 }
+}
+
+const dist = 'dist'
+
+await mkdir(dist, { recursive: true })
+
+// lucide --------------------------------------------------------------------------------------------------------------
+
+const lucideIcons = await Promise.all((await searchSvgs('lucide/icons')).map(loadSvg))
+
+await writeFile(`${dist}/lucide.tsx`, `type Icon = (props: React.SVGProps<SVGSVGElement>) => React.ReactElement
+
+const LucideIcon: Icon = props => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props} />
+)
+${lucideIcons.map(({ name, svg, base64 }) => `
+/** ![](data:image/svg+xml;base64,${base64}) */
+export const ${name}Icon: Icon = props => (
+  <LucideIcon {...props}>
+    ${svg.slice(203, svg.indexOf('\n</svg>')).replace(/  /g, '    ')}
+  </LucideIcon>
+)
+`).join('')}
+`)
+
+// simple --------------------------------------------------------------------------------------------------------------
+
+const simpleIcons = await Promise.all((await searchSvgs('simple-icons/icons')).map(loadSvg))
+
+await writeFile(`${dist}/simple.tsx`, `type Icon = (props: React.SVGProps<SVGSVGElement>) => React.ReactElement
+
+const SimpleIcon: Icon = props => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props} />
+${simpleIcons.map(({ name, svg, base64 }) => `
+/** ![](data:image/svg+xml;base64,${base64}) */
+export const Brand${name}: Icon = props => (
+  <SimpleIcon {...props}>
+    ${svg.slice(svg.indexOf('</title>') + 8, svg.indexOf('</svg>'))}
+  </SimpleIcon>
+)
+`).join('')}
+`)
