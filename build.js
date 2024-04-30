b980ddf9cb8d2cd05cd7c87db749009d4e51a50d
zce <w@zce.me>
2024-04-30 10:54:59 +0800

    feat: heroicons

--- build.js
+++ build.js
@@ -12,7 +12,7 @@ import { basename } from 'node:path'
  * @param {string} dir svgs directory
  * @returns {Promise<string[]>} a list of svg filenames
  */
-const searchSvgs = async (dir) => {
+const searchSvgs = async dir => {
   const dirents = await readdir(dir, { withFileTypes: true })
   return dirents.filter(d => d.isFile() && d.name.endsWith('.svg')).map(d => `${dir}/${d.name}`)
 }
@@ -22,11 +22,16 @@ const searchSvgs = async (dir) => {
  * @param {string} path svg filename
  * @returns {Promise<Icon>} An icon object
  */
-const loadSvg = async (path) => {
+const loadSvg = async path => {
   const camel = basename(path, '.svg').replace(/-([a-z0-9])/g, (_, letter) => letter.toUpperCase())
   const name = camel[0].toUpperCase() + camel.slice(1)
-  const svg = await readFile(path,'utf8')
-  const base64 = Buffer.from(svg.replace(/currentColor/g, '#64748b').replace('width="24"', 'width="50px"').replace('height="24"', 'height="50px"')).toString('base64')
+  const svg = (await readFile(path, 'utf8')).replace(/#0F172A/g, 'currentColor')
+  const base64 = Buffer.from(
+    svg
+      .replace(/currentColor/g, '#64748b')
+      .replace('width="24"', 'width="50px"')
+      .replace('height="24"', 'height="50px"')
+  ).toString('base64')
   return { name, svg, base64 }
 }
 
@@ -38,32 +43,74 @@ await mkdir(dist, { recursive: true })
 
 const lucideIcons = await Promise.all((await searchSvgs('lucide/icons')).map(loadSvg))
 
-await writeFile(`${dist}/lucide.tsx`, `
+await writeFile(
+  `${dist}/lucide.tsx`,
+  `
 const LucideIcon: Icon = props => (
  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props} />
 )
-${lucideIcons.sort((a, b) => a.name.localeCompare(b.name)).map(({ name, svg, base64 }) => `
+${lucideIcons
+  .sort((a, b) => a.name.localeCompare(b.name))
+  .map(
+    ({ name, svg, base64 }) => `
 /** ![](data:image/svg+xml;base64,${base64}) */
 export const ${name}Icon: Icon = props => (
   <LucideIcon {...props}>
     ${svg.slice(203, svg.indexOf('\n</svg>')).replace(/  /g, '    ')}
   </LucideIcon>
 )
-`).join('')}
-`)
+`
+  )
+  .join('')}
+`
+)
+
+// heroicons --------------------------------------------------------------------------------------------------------------
+
+const heroIcons = await Promise.all((await searchSvgs('heroicons/src/24/outline')).map(loadSvg))
+
+await writeFile(
+  `${dist}/heroicons.tsx`,
+  `
+const HeroIcon: Icon = props => (
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props} />
+)
+${heroIcons
+  .sort((a, b) => a.name.localeCompare(b.name))
+  .map(
+    ({ name, svg, base64 }) => `
+/** ![](data:image/svg+xml;base64,${base64}) */
+export const ${name}Icon: Icon = props => (
+  <HeroIcon {...props}>
+    ${svg.slice(96, svg.indexOf('\n</svg>')).replace(/\n/g, '\n    ')}
+  </HeroIcon>
+)
+`
+  )
+  .join('')}
+`
+)
 
 // simple --------------------------------------------------------------------------------------------------------------
 
 const simpleIcons = await Promise.all((await searchSvgs('simple-icons/icons')).map(loadSvg))
 
-await writeFile(`${dist}/simple.tsx`, `
+await writeFile(
+  `${dist}/simple.tsx`,
+  `
 const SimpleIcon: Icon = props => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props} />
-${simpleIcons.sort((a, b) => a.name.localeCompare(b.name)).map(({ name, svg, base64 }) => `
+${simpleIcons
+  .sort((a, b) => a.name.localeCompare(b.name))
+  .map(
+    ({ name, svg, base64 }) => `
 /** ![](data:image/svg+xml;base64,${base64}) */
 export const Brand${name}: Icon = props => (
   <SimpleIcon {...props}>
     ${svg.slice(svg.indexOf('</title>') + 8, svg.indexOf('</svg>'))}
   </SimpleIcon>
 )
-`).join('')}
-`)
+`
+  )
+  .join('')}
+`
+)
